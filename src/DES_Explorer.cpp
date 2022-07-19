0abe5d04819a713e5f98441adc6c296f09f4d730
Eveheeero <xhve00000@naver.com>
2022-07-20 01:10:27 +0900

    DES_Explorer 주석 추가

--- src/DES_Explorer.cpp
+++ src/DES_Explorer.cpp
@@ -1,6 +1,18 @@
+/**
+ * @file DES_Explorer.cpp
+ * @author 정종민 (xhve00000@gmail.com)
+ * @brief 메인 클래스 소스파일입니다.
+ * @version 0.1
+ * @date 2022-07-20
+ *
+ * @copyright Copyright (c) 2022
+ * @note author 및 date 관련해서는, 수정 시 추가해주세요.
+ * @details 소스파일은 기능별로 분리하는것이 좋습니다.
+ */
+
 #include "include/DES_Explorer.hpp"
 
 std::string DES_Explorer::get_current_path()
 {
-    return current_path.string(); /** 현재 탐색기의 경로를 반환한다. */
+  return current_path.string(); /** 현재 탐색기의 경로를 반환한다. */
 }
