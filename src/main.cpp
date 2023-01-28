74f910cbe4feef02447b5f74c26da4aeebad0db7
Eveheeero <xhve00000@naver.com>
2023-01-28 14:08:19 +0000

    curse 라이브러리 삭제 및 ftxui 라이브러리 적용

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include "DES_Explorer.hpp"
+#include <ftxui/screen/screen.hpp>
 
 int
 main(int argc, char** argv)
 {
-  initscr();
-  printw("Hello!\n");
-  printw("ncurses sample!");
-  getch();
-  endwin();
+  ftxui::Screen screen(30, 30);
+  screen.Print();
   return 0;
 }
